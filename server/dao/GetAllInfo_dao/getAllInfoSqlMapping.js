1278d3dc4eeb9c5fa0949796d3516af544ca38ef
unknown <he1275765531@sohu.com>
2017-09-29 15:27:26 +0800

    'sql'

--- server/dao/GetAllInfo_dao/getAllInfoSqlMapping.js
+++ server/dao/GetAllInfo_dao/getAllInfoSqlMapping.js
@@ -1,6 +1,5 @@
 var getAllInfo = {
   getAllInfo: function (param) {
-    /*数据查询*/
     return $sql = 'SELECT U.cont_id AS CONTID,U.contents,C.NAME FROM user_object_common U JOIN content C  JOIN node_cont NC  WHERE U.cont_id = C.contid  AND NC.cont_id = U.cont_id AND NC.node_id =' + param;
   }
 }
